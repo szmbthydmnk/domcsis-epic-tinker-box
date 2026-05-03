0deccccac3236a42dec5e1187b5045cffed94aaa
Dominik Szombathy <szmbthy.dmnk@gmail.com>
2026-05-03 20:47:14 +0200

    x+1 eigenstate definition. mypy playing/
    v0.0.0 - Tested mypy for a few lines and played with it.

---
+++ python/src/quantum_states/one_qubit_stabilizer_states.py
@@ -0,0 +1,24 @@
+# This code is part of Domcsi's Epic Tinker Box
+#
+#
+
+import numpy as np
+
+def one_qubit_stabilizer_state(identifier: str) -> np.ndarray:
+    """
+    
+    """
+    
+    if not isinstance(id, str):
+        raise TypeError(f"identifier must be of type string, got {type(identifier).__name__}")
+    if id == "":
+        raise ValueError(f"identifier was empty")
+    
+    if identifier == "x":
+        return _x_positive_eigenstate()
+
+
+def _x_positive_eigenstate() -> np.ndarray:
+    """ Internal helper; gives back the  """
+    return np.array(1/np.sqrt(2) * np.array([1, 1]))
+    
