d946ebf4c36736a85ee2fb2132f6621502b6a5c8
Dominik Szombathy <szmbthy.dmnk@gmail.com>
2026-05-19 11:52:33 +0200

    added stub for PauliOperator class

--- python/src/domcsis_epic_tinker_box/pauli_algebra/__init__.py
+++ python/src/domcsis_epic_tinker_box/pauli_algebra/__init__.py
@@ -1,7 +1,8 @@
-from .pauli_operators import pauli_matrix, generate_all_pauli_strings, generate_pauli_operators
+from .pauli_operators import pauli_matrix, generate_all_pauli_strings, generate_pauli_operators, PauliOperators
 
 __all__ = [
     "pauli_matrix",
     "generate_all_pauli_strings",
     "generate_pauli_operators",
+    "PauliOperators",
 ]
