d1203efaa245be7c31458697b616fefd7f1c1ae4
Dominik Szombathy <szmbthy.dmnk@gmail.com>
2026-05-23 17:08:29 +0200

    Update make.jl

--- julia/docs/make.jl
+++ julia/docs/make.jl
@@ -1,20 +1,15 @@
 using Documenter
 using DomcsisEpicTinkerBox
 
-Documenter.makedocs(
-    sitename = "DomcsisEpicTinkerBox.jl",
-    modules  = [DomcsisEpicTinkerBox],
-    format   = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-    ),
-    pages = [
-        "Home" => "index.md",
-        "API"  => "api.md",
-    ],
+Documenter.makedocs(;
+    sitename="DomcsisEpicTinkerBox.jl",
+    modules=[DomcsisEpicTinkerBox],
+    format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
+    pages=["Home" => "index.md", "API" => "api.md"],
 )
 
-Documenter.deploydocs(
-    repo   = "github.com/szmbthydmnk/domcsis-epic-tinker-box.git",
-    branch = "gh-pages",
-    devbranch = "main",
+Documenter.deploydocs(;
+    repo="github.com/szmbthydmnk/domcsis-epic-tinker-box.git",
+    branch="gh-pages",
+    devbranch="main",
 )
